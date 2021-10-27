315a74cd4eb5e801891faf493b6aaed46f025b2e
Piccodi <akakoa@yandex.ru>
2021-10-27 23:28:32 +0300

    init

--- src/main/java/events/SendEvent.java
+++ src/main/java/events/SendEvent.java
@@ -10,57 +10,62 @@ public class SendEvent extends ListenerAdapter {
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event){
 
-        String mess = event.getMessage().getContentRaw();
+        if(event.getAuthor().isBot()) event.getChannel().sendMessage("u can't catch me").queue();
 
-        if(mess.startsWith("!help")){
-            String sending = "to send 1 picture write: (#) \"!send meme\" " +
-                    " to send more use \";\" after request: (#) \"!send meme ; 10\" ";
-            event.getChannel().sendMessage(sending).queue();
-        }
+        else {
 
-        if(mess.startsWith("!send")){
+            String mess = event.getMessage().getContentRaw();
 
-            String query;
-            Integer num;
-
-            if(mess.contains(";")){
-
-                if(mess.substring(mess.indexOf(";") - 1, mess.indexOf(";")).contains(" "))
-                    query = mess.substring(mess.indexOf(" ") + 1, mess.indexOf(";") - 1);
-                else query = mess.substring(mess.indexOf(" ") + 1, mess.indexOf(";"));
-
-                if(mess.substring(mess.indexOf(";")).length() > 1){
-                    String count = mess.substring(mess.indexOf(";") + 2);
-                    if(NumberUtils.isDigits(count)) num = Integer.valueOf(count);
-                    else num = 1;
-                }
-                else num = 1;
-            }
-            else{
-                query = mess.substring(mess.indexOf(" ") + 1);
-                num = 1;
+            if (mess.startsWith("!help")) {
+                String sending = "to send 1 picture write: (#) \"!send meme\" " +
+                        " to send more use \";\" after request: (#) \"!send meme ; 10\" ";
+                event.getChannel().sendMessage(sending).queue();
             }
 
-            var parser = new ParsePictures();
-            parser.setUrl(query);
+            if (mess.startsWith("!send")) {
 
-            List<String> urls = parser.parsing(num);
+                String query;
+                Integer num;
+                if (mess.equals("!send") || mess.equals("!send "))
+                    event.getChannel().sendMessage("empty query").queue();
+                else {
+                    if (mess.contains(";")) {
 
-            for (String url : urls) System.out.println(url);
+                        if (mess.substring(mess.indexOf(";") - 1, mess.indexOf(";")).contains(" "))
+                            query = mess.substring(mess.indexOf(" ") + 1, mess.indexOf(";") - 1);
+                        else query = mess.substring(mess.indexOf(" ") + 1, mess.indexOf(";"));
 
-            if(num > 1) {
-                for (int i = 0; i < num + num; i++) {
-                    if(i >= urls.size()){
-                        event.getChannel().sendMessage( "выведено: " + urls.size()/2 + " ").queue();
-                        break;
+                        if (mess.substring(mess.indexOf(";")).length() > 1) {
+                            String count = mess.substring(mess.indexOf(";") + 2);
+                            if (NumberUtils.isDigits(count)) num = Integer.valueOf(count);
+                            else num = 1;
+                        } else num = 1;
+                    } else {
+                        query = mess.substring(mess.indexOf(" ") + 1);
+                        num = 1;
                     }
-                    event.getChannel().sendMessage(urls.get(i)).queue();
-                }
-            }else{
-                event.getChannel().sendMessage(urls.get(0)).queue();
-                event.getChannel().sendMessage(urls.get(1)).queue();
-            }
 
+                    var parser = new ParsePictures();
+                    parser.setUrl(query);
+
+                    List<String> urls = parser.parsing(num);
+
+                    for (String url : urls) System.out.println(url);
+
+                    if (num > 1) {
+                        for (int i = 0; i < num + num; i++) {
+                            if (i >= urls.size()) {
+                                event.getChannel().sendMessage("выведено: " + urls.size() / 2 + " ").queue();
+                                break;
+                            }
+                            event.getChannel().sendMessage(urls.get(i)).queue();
+                        }
+                    } else {
+                        event.getChannel().sendMessage(urls.get(0)).queue();
+                        if (urls.size() > 1) event.getChannel().sendMessage(urls.get(1)).queue();
+                    }
+                }
+            }
         }
 
     }
