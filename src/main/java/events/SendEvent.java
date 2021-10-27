1343c970bb1492ecd9e60b17650f506aa03aa491
Piccodi <akakoa@yandex.ru>
2021-10-27 23:31:41 +0300

    init

--- src/main/java/events/SendEvent.java
+++ src/main/java/events/SendEvent.java
@@ -10,60 +10,58 @@ public class SendEvent extends ListenerAdapter {
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event){
 
-        if(event.getAuthor().isBot()) event.getChannel().sendMessage("u can't catch me").queue();
 
-        else {
 
-            String mess = event.getMessage().getContentRaw();
+        String mess = event.getMessage().getContentRaw();
 
-            if (mess.startsWith("!help")) {
-                String sending = "to send 1 picture write: (#) \"!send meme\" " +
-                        " to send more use \";\" after request: (#) \"!send meme ; 10\" ";
-                event.getChannel().sendMessage(sending).queue();
-            }
+        if(mess.startsWith("!help")){
+            String sending = "to send 1 picture write: (#) \"!send meme\" " +
+                    " to send more use \";\" after request: (#) \"!send meme ; 10\" ";
+            event.getChannel().sendMessage(sending).queue();
+        }
 
-            if (mess.startsWith("!send")) {
+        if(mess.startsWith("!send")){
 
-                String query;
-                Integer num;
-                if (mess.equals("!send") || mess.equals("!send "))
-                    event.getChannel().sendMessage("empty query").queue();
-                else {
-                    if (mess.contains(";")) {
+            String query;
+            Integer num;
 
-                        if (mess.substring(mess.indexOf(";") - 1, mess.indexOf(";")).contains(" "))
-                            query = mess.substring(mess.indexOf(" ") + 1, mess.indexOf(";") - 1);
-                        else query = mess.substring(mess.indexOf(" ") + 1, mess.indexOf(";"));
+            if(event.getAuthor().isBot()) event.getChannel().sendMessage("u can't catch me").queue();
+            else if(mess.equals("!send") || mess.equals("!send ")) event.getChannel().sendMessage("empty query").queue();
+            else {
+                if (mess.contains(";")) {
 
-                        if (mess.substring(mess.indexOf(";")).length() > 1) {
-                            String count = mess.substring(mess.indexOf(";") + 2);
-                            if (NumberUtils.isDigits(count)) num = Integer.valueOf(count);
-                            else num = 1;
-                        } else num = 1;
-                    } else {
-                        query = mess.substring(mess.indexOf(" ") + 1);
-                        num = 1;
-                    }
+                    if (mess.substring(mess.indexOf(";") - 1, mess.indexOf(";")).contains(" "))
+                        query = mess.substring(mess.indexOf(" ") + 1, mess.indexOf(";") - 1);
+                    else query = mess.substring(mess.indexOf(" ") + 1, mess.indexOf(";"));
 
-                    var parser = new ParsePictures();
-                    parser.setUrl(query);
+                    if (mess.substring(mess.indexOf(";")).length() > 1) {
+                        String count = mess.substring(mess.indexOf(";") + 2);
+                        if (NumberUtils.isDigits(count)) num = Integer.valueOf(count);
+                        else num = 1;
+                    } else num = 1;
+                } else {
+                    query = mess.substring(mess.indexOf(" ") + 1);
+                    num = 1;
+                }
 
-                    List<String> urls = parser.parsing(num);
+                var parser = new ParsePictures();
+                parser.setUrl(query);
 
-                    for (String url : urls) System.out.println(url);
+                List<String> urls = parser.parsing(num);
 
-                    if (num > 1) {
-                        for (int i = 0; i < num + num; i++) {
-                            if (i >= urls.size()) {
-                                event.getChannel().sendMessage("выведено: " + urls.size() / 2 + " ").queue();
-                                break;
-                            }
-                            event.getChannel().sendMessage(urls.get(i)).queue();
+                for (String url : urls) System.out.println(url);
+
+                if (num > 1) {
+                    for (int i = 0; i < num + num; i++) {
+                        if (i >= urls.size()) {
+                            event.getChannel().sendMessage("выведено: " + urls.size() / 2 + " ").queue();
+                            break;
                         }
-                    } else {
-                        event.getChannel().sendMessage(urls.get(0)).queue();
-                        if (urls.size() > 1) event.getChannel().sendMessage(urls.get(1)).queue();
+                        event.getChannel().sendMessage(urls.get(i)).queue();
                     }
+                } else {
+                    event.getChannel().sendMessage(urls.get(0)).queue();
+                    if(urls.size() > 1 )event.getChannel().sendMessage(urls.get(1)).queue();
                 }
             }
         }
